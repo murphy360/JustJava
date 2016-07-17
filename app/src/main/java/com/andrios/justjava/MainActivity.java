a254e30690cfa6db4d6540e6c480748d7f6e3e88
Corey Murphy <c.murphy360@gmail.com>
2016-07-16 20:11:52 -0700

    Many changes including styling, theming and exporting text to strings.

--- app/src/main/java/com/andrios/justjava/MainActivity.java
+++ app/src/main/java/com/andrios/justjava/MainActivity.java
@@ -1,8 +1,12 @@
 package com.andrios.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -12,7 +16,9 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends ActionBarActivity {
     int numberOfCoffees = 3;
-    int price = 5;
+    int pricePerCup = 5;
+    int priceWhipped = 1;
+    int priceChocolate = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +30,34 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Total: $" + (numberOfCoffees * price) + "\n" + "Thank You!";
-        displayMessage(priceMessage);
+        CheckBox whippedCheckbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean isWhipped = whippedCheckbox.isChecked();
+
+        CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean isChocolate = chocolateCheckbox.isChecked();
+
+        int price = calculatePrice(isWhipped, isChocolate);
+        String priceMessage = createOrderSummary(price, isWhipped, isChocolate);
+
+        //Open Email application with order details
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, "c.murphy360@gmail.com");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "New Order");
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+
+
+
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
@@ -40,33 +66,76 @@ public class MainActivity extends ActionBarActivity {
 
 
     public void increment(View view){
-        numberOfCoffees++;
-       display(numberOfCoffees);
-        displayPrice(numberOfCoffees*price);
+        if(numberOfCoffees <100){
+            numberOfCoffees++;
+            displayQuantity(numberOfCoffees);
+            displayPrice();
+        }
+
+
     }
 
     public void decriment(View view){
         if (numberOfCoffees > 0) {
             numberOfCoffees--;
-            display(numberOfCoffees);
-            displayPrice(numberOfCoffees*price);
+            displayQuantity(numberOfCoffees);
+            displayPrice();
+
         };
+    }
+
+    public void displayPrice(View v){
+        displayPrice();
     }
 
     /**
      * This method displays the given text on the screen.
      * */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+    private void displayPrice() {
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        CheckBox whippedCheckbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean isWhipped = whippedCheckbox.isChecked();
+
+        CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean isChocolate = chocolateCheckbox.isChecked();
+
+        String message = "$" + calculatePrice(isWhipped, isChocolate);
         priceTextView.setText(message);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
 
+    /**
+     *
+     * @param price
+     * @return String with formatted message
+     */
+    private String createOrderSummary(int price, boolean isWhippedCream, boolean isChocolate){
+        EditText editNameText = (EditText) findViewById(R.id.edit_name_text);
+
+        String priceMessage =
+                "Name: " + editNameText.getText() + "\n" +
+                        "Add whipped cream? " + isWhippedCream + "\n" +
+                        "Add Chocolate? " + isChocolate + "\n" +
+                        "Quantity: " + numberOfCoffees + "\n" +
+                        "Total: $" + (price) + "\n" +
+                        "Thank You!";
+        return priceMessage;
+    }
+    /*Calculate price of coffee
+     *
+     * @returns price
+     */
+    private int calculatePrice(boolean isWhipped, boolean isChocolate){
+      int price = pricePerCup;
+
+        if(isWhipped){
+
+           price += priceWhipped;
+        }
+
+        if (isChocolate){
+            price += priceChocolate;
+        }
+        return (price * numberOfCoffees);
+    }
 }
